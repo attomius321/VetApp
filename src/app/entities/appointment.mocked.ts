dddecf5a4beadf080b86702433d404d11f4d3120
attomius321 <antonyotopolniceanu@gmail.com>
2020-07-08 10:20:01 -0700

    Added firebase config

---
+++ src/app/entities/appointment.mocked.ts
@@ -0,0 +1,28 @@
+import {Appointment} from "./appointment.model";
+
+export let APPOINTMENTS: Array<Appointment> =[
+  {
+    id: '231312',
+    animal: 'Doggo',
+    timestamp: 193823823232,
+    doc: 'Fierarul',
+    diagnostic: 'Sanatate tie noroc mie',
+    status: 0
+  },
+  {
+    id: '2312321',
+    animal: 'Doggo',
+    timestamp: 321321321213,
+    doc: 'Fierarul',
+    diagnostic: 'Sanatate tie noroc mie',
+    status: 2
+  },
+  {
+    id: '232',
+    animal: 'Mangusta',
+    timestamp: 231241232133,
+    doc: 'Fierarul',
+    diagnostic: 'Sanatate tie noroc mie',
+    status: 1
+  }
+]
---
+++ src/app/entities/appointment.model.ts
@@ -0,0 +1,8 @@
+export interface Appointment {
+  id: string;
+  animal: string;
+  timestamp: number;
+  doc: string;
+  diagnostic: string;
+  status: number;
+}

78c694efa7c3f3340e0f6110487dd8257bb925cf
attomius321 <antonyotopolniceanu@gmail.com>
2020-07-21 06:19:14 -0700

    Some changes on homepage

--- src/app/homepage/homepage-component/homepage.component.ts
+++ src/app/homepage/homepage-component/homepage.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth/auth.service";
+import {INavigationService, NAVIGATION_SERVICE} from "../../services/navigation/navigation.service";
 
 @Component({
   selector: 'app-homepage',
@@ -7,10 +8,15 @@ import {AuthService} from "../../services/auth/auth.service";
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
-  constructor(public auth: AuthService) { }
+  title="VetApp";
+  constructor(public auth: AuthService,
+              @Inject(NAVIGATION_SERVICE) private navService: INavigationService) { }
 
   ngOnInit(): void {
+  }
+
+  goToAppointments(){
+    this.navService.openAppointments();
   }
 
 }
